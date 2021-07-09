8c50caa716ebe21de76e9dac443cf6b5a2f769f1
Kalasouski <kolosovsky2002@gmail.com>
2021-07-09 12:21:24 +0300

    code reformatted

--- src/test/java/oop/task29/SimpleCalculatorTest.java
+++ src/test/java/oop/task29/SimpleCalculatorTest.java
@@ -1,14 +1,14 @@
 package oop.task29;
 
 
-import oop.task29.SimpleCalculator;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class SimpleCalculatorTest {
--- src/test/java/oop/task33/CarpetTest.java
+++ src/test/java/oop/task33/CarpetTest.java
@@ -1,14 +1,13 @@
 package oop.task33;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CarpetTest {
     @ParameterizedTest(name = "When cost={0} then exception")
-    @ValueSource(doubles = {-1,-100})
+    @ValueSource(doubles = {-1, -100})
     void givenCarpetConstructorWhenSettingNegativeValueThenThrowsException(double cost) {
         assertThrows(IllegalArgumentException.class, () -> new Carpet(cost));
     }
