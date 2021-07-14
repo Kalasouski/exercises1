76811aeba27c099fa47c35dca2e900d3fea17854
Kalasouski <kolosovsky2002@gmail.com>
2021-07-14 10:00:00 +0300

    Implement task 50
    vocabulary added

--- src/main/java/oop/task50/Main.java
+++ src/main/java/oop/task50/Main.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 
 public class Main {
     private final Map<Integer, Location> locations = new HashMap<>();
+    private final Map<String, String> vocabulary = new HashMap<>();
     Scanner scanner = new Scanner(System.in);
 
     public Main() {
@@ -33,6 +34,12 @@ public class Main {
 
         locations.get(5).addExit("S", 1);
         locations.get(5).addExit("W", 2);
+
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("EAST", "E");
     }
 
     public void command() {
@@ -49,11 +56,20 @@ public class Main {
             }
             System.out.println();
             String direction = scanner.nextLine().toUpperCase();
+            if (direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for (String word : words) {
+                    if (vocabulary.containsKey(word)) {
+                        direction = vocabulary.get(word);
+                        break;
+                    }
+                }
+            }
             if (exits.containsKey(direction)) {
                 loc = exits.get(direction);
+            } else {
+                System.out.println("You cannot go in that direction");
             }
-
         }
-        // write code here
     }
 }
